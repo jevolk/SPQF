588db2df4c2600650b7ea44d4b385267e37890f0
jzk <jzk@freenode.memoserv>
2014-09-28 22:55:24 -0700

    Modify ResPublica derived logic class with preliminary testing skeleton.

--- respub.cpp
+++ respub.cpp
@@ -39,62 +39,118 @@
 #include "respub.h"
 
 
-/*
-
-void ResPublica::handle_join(const Msg &msg)
+void ResPublica::handle_mode(const Msg &msg,
+                             Chan &c)
 {
-	log_handle(msg,"JOIN");
+
+
 }
 
 
-void ResPublica::handle_part(const Msg &msg)
+
+void ResPublica::handle_mode(const Msg &msg,
+                             Chan &c,
+                             User &u)
 {
-	log_handle(msg,"PART");
+
+
 }
 
 
-void ResPublica::handle_mode(const Msg &msg)
+void ResPublica::handle_join(const Msg &msg,
+                             Chan &c,
+                             User &u)
 {
-	log_handle(msg,"MODE");
+
+
 }
 
 
-void ResPublica::handle_umode(const Msg &msg)
+void ResPublica::handle_part(const Msg &msg,
+                             Chan &c,
+                             User &u)
 {
-	log_handle(msg,"UMODE");
+
+
 }
 
 
-void ResPublica::handle_kick(const Msg &msg)
+void ResPublica::handle_kick(const Msg &msg,
+                             Chan &c,
+                             User &u)
 {
-	log_handle(msg,"KICK");
+
+
 }
 
 
-void ResPublica::handle_channel(const Msg &msg)
+void ResPublica::handle_cnotice(const Msg &msg,
+                                Chan &c,
+                                User &u)
 {
-	log_handle(msg,"CHANNEL");
 
+
+}
+
+
+void ResPublica::handle_chanmsg(const Msg &msg,
+                                Chan &c,
+                                User &u)
+{
 	using delim = boost::char_separator<char>;
 	const delim sep(" ");
 
-	const std::string &channel = params.at(0);
-	const std::string &text = params.at(1);
+	const std::string &text = msg[CHANMSG::TEXT];
 	const boost::tokenizer<delim> toks(text,sep);
 	const std::vector<std::string> tokens(toks.begin(),toks.end());
+
+	if(tokens.at(0) == "!accounttest")
+	{
+		c.for_each([&]
+		(const Chan::UsersVal &val)
+		{
+			const User &user = *std::get<0>(val);
+			std::stringstream s;
+			s << user.get_nick() << " is" << (!user.is_logged_in()? " not " : " ") << "logged in";
+			if(user.get_nick() == u.get_nick())
+				s << " and made this query.";
+
+			c.msg(s.str());
+		});
+
+		return;
+	}
 
 	if(tokens.at(0) == "!votekick") try
 	{
 		const std::string &target = tokens.at(1);
-		handle_votekick(target);
+		c.msg("Thanks for voting");
 	}
 	catch(const std::out_of_range &e)
 	{
-		reply_chan("Try using !votekick <nickname> to cast your vote.");
+		c.msg("Try using !votekick <nickname> to cast your vote.");
 		return;
 	}
 }
 
+
+void ResPublica::handle_notice(const Msg &msg,
+                               User &u)
+{
+
+
+}
+
+
+void ResPublica::handle_privmsg(const Msg &msg,
+                                User &u)
+{
+
+
+}
+
+
+/*
 void ResPublica::handle_votekick(const std::string &target)
 {
 	if(target == my_nick())
@@ -121,62 +177,10 @@ void ResPublica::handle_votekick(const std::string &target)
 	if(votes >= thresh)
 		kick(target,"The people have spoken.");
 }
-
-void ResPublica::handle_privmsg(const Msg &msg)
-{
-	log_handle(msg,"PRIVMSG");
-}
+*/
 
 
-void ResPublica::handle_notice(const Msg &msg)
-{
-	log_handle(msg,"NOTICE");
-}
-
-
-void ResPublica::handle_chan_notice(const Msg &msg)
-{
-	log_handle(msg,"CHANNEL NOTICE");
-}
-
-
-void ResPublica::handle_namreply(const Msg &msg)
-{
-	log_handle(msg,"NAME REPLY");
-
-	const std::string &self = params.at(0);
-	const std::string &equals_sign = params.at(1);
-	const std::string &channel = params.at(2);
-
-	std::for_each(params.begin()+3,params.end(),[&]
-	(const std::string &names)
-	{
-		using delim = boost::char_separator<char>;
-		const delim sep(" ");
-		const boost::tokenizer<delim> tokens(names,sep);
-		for(const auto &nick: tokens)
-		{
-			const std::string &n = nick_has_flags(nick)? nick.substr(1) : nick;
-			users.emplace(std::piecewise_construct,
-			              std::make_tuple(n),
-			              std::make_tuple());
-		}
-	});
-}
-
-
-void ResPublica::handle_endofnames(const Msg &msg)
-{
-	log_handle(msg,"END OF NAMES");
-
-	printf("Have %zu users\n",users.size());
-	for(const auto &p : users)
-		std::cout << p.first << " ";
-
-	std::cout << std::endl;
-}
-
-
+/*
 size_t ResPublica::inc_votes_against(const std::string &nick)
 try
 {
--- respub.h
+++ respub.h
@@ -14,19 +14,16 @@ class ResPublica : public Bot
 
 	//void handle_votekick(const std::string &target);
 
-/*
-	void handle_endofnames(const Msg &msg) override;
-	void handle_namreply(const Msg &msg) override;
-	void handle_chan_notice(const Msg &msg) override;
-	void handle_notice(const Msg &msg) override;
-	void handle_privmsg(const Msg &msg) override;
-	void handle_channel(const Msg &msg) override;
-	void handle_kick(const Msg &msg) override;
-	void handle_umode(const Msg &msg) override;
-	void handle_mode(const Msg &msg) override;
-	void handle_part(const Msg &msg) override;
-	void handle_join(const Msg &msg) override;
-*/
+	void handle_privmsg(const Msg &m, User &u) override;
+	void handle_notice(const Msg &m, User &u) override;
+
+	void handle_chanmsg(const Msg &m, Chan &c, User &u) override;
+	void handle_cnotice(const Msg &m, Chan &c, User &u) override;
+	void handle_kick(const Msg &m, Chan &c, User &u) override;
+	void handle_part(const Msg &m, Chan &c, User &u) override;
+	void handle_join(const Msg &m, Chan &c, User &u) override;
+	void handle_mode(const Msg &m, Chan &c, User &u) override;
+	void handle_mode(const Msg &m, Chan &c) override;
 
   public:
 	template<class... Args> ResPublica(Args&&... args): Bot(std::forward<Args>(args)...) {}
