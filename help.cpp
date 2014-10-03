7bb892d3319275ed11f20f5c4589a00f2914a4c4
jzk <jzk@freenode.memoserv>
2014-10-02 18:41:05 -0700

    Fix help externs.

--- help.cpp
+++ help.cpp
@@ -6,7 +6,7 @@
  */
 
 
-extern const char help_vote[] = \
+extern const char *const help_vote = \
 "*** SENATVS POPVLVS QVE FREENODUS - The Senate and The People of Freenode ( #SPQF )\n"\
 "*** Voting System:\n"\
 "*** usage:    !vote   <command | issue>       : Initiate a vote for an issue or supply a command...\n"\
@@ -16,20 +16,20 @@ extern const char help_vote[] = \
 "*** command:  config  [variable ...]          : Show whole configuration or one or more variable\n"\
 ;
 
-extern const char help_vote_config[] = \
+extern const char *const help_vote_config = \
 "** Vote on changing various configuration settings.\n"\
 "** The syntax for initiating a vote is: <variable> = <new value>\n"\
 "** The alternative syntax for printing information is: <variable> [variable ...]\n"\
 "** Example: !vote config vote_quorum_minimum = 10  : requires at least 10 participants in any vote\n"\
 ;
 
-extern const char help_vote_kick[] = \
+extern const char *const help_vote_kick = \
 "** Vote to kick user(s) from the channel.\n"\
 "** The syntax for initiating a vote is: [target ...]\n"\
 "** Example: !vote kick fred waldo             : Starts a single vote to kick both fred and waldo"\
 ;
 
-extern const char help_vote_mode[] = \
+extern const char *const help_vote_mode = \
 "** Vote on changing modes for a channel with operator syntax.\n"\
 "** The syntax for initiating a vote is: <mode string> [target ...]\n"\
 "** Example: !vote +q $a:foobar     : Quiets based on the account foobar\n"\
