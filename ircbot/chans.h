32d43c2fa68f477f32c9d9b4751f4c476cf9a994
jzk <jzk@freenode.memoserv>
2014-10-09 22:13:16 -0700

    Channel names tolower().

--- ircbot/chans.h
+++ ircbot/chans.h
@@ -18,8 +18,8 @@ class Chans
 	const Adb &get_adb() const                         { return adb;                                }
 	const Sess &get_sess() const                       { return sess;                               }
 
-	const Chan &get(const std::string &name) const     { return chans.at(name);                     }
-	bool has(const std::string &name) const            { return chans.count(name);                  }
+	const Chan &get(const std::string &name) const     { return chans.at(tolower(name));            }
+	bool has(const std::string &name) const            { return chans.count(tolower(name));         }
 	size_t num() const                                 { return chans.size();                       }
 
 	// Closures
@@ -30,7 +30,7 @@ class Chans
 	Chan &get(const std::string &name);                // throws Exception
 	Chan &add(const std::string &name);                // Add channel (w/o join) or return existing
 	Chan &join(const std::string &name);               // Add channel with join or return existing
-	bool del(const std::string &name)                  { return chans.erase(name);                  }
+	bool del(const std::string &name)                  { return chans.erase(tolower(name));         }
 	bool del(const Chan &chan)                         { return del(chan.get_name());               }
 	void autojoin();                                   // Joins all channels in the autojoin list
 
@@ -63,7 +63,7 @@ void Chans::autojoin()
 inline
 Chan &Chans::join(const std::string &name)
 {
-	Chan &chan = add(name);
+	Chan &chan = add(tolower(name));
 	chan.join();
 	return chan;
 }
@@ -73,12 +73,12 @@ inline
 Chan &Chans::get(const std::string &name)
 try
 {
-	return chans.at(name);
+	return chans.at(tolower(name));
 }
 catch(const std::out_of_range &e)
 {
 	std::stringstream s;
-	s << "Unrecognized channel name [" << name << "]";
+	s << "Unrecognized channel name [" << tolower(name) << "]";
 	throw Exception(s.str());
 }
 
@@ -87,8 +87,8 @@ inline
 Chan &Chans::add(const std::string &name)
 {
 	auto iit = chans.emplace(std::piecewise_construct,
-	                         std::forward_as_tuple(name),
-	                         std::forward_as_tuple(adb,sess,name));
+	                         std::forward_as_tuple(tolower(name)),
+	                         std::forward_as_tuple(adb,sess,tolower(name)));
 
 	Chan &chan = iit.first->second;
 	return chan;
