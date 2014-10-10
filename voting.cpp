4eb91d8cb071a56c8ecebb33f8a476e516b0823f
jzk <jzk@freenode.memoserv>
2014-10-10 01:44:57 -0700

    Deinline Voting manager related.

--- voting.cpp
+++ voting.cpp
@@ -40,6 +40,34 @@ Voting::~Voting() noexcept
 }
 
 
+void Voting::cancel(const id_t &id,
+                    const Chan &chan,
+                    const User &user)
+{
+	Vote &vote = get(id);
+	cancel(vote,chan,user);
+}
+
+
+void Voting::cancel(Vote &vote,
+                    const Chan &chan,
+                    const User &user)
+{
+	if(user.get_acct() != vote.get_user_acct())
+	{
+		std::stringstream ss;
+		ss << "You can't cancel a vote by " << vote.get_user_acct() << ".";
+		throw Exception(ss.str());
+	}
+
+	if(vote.total() > 0)
+		throw Exception("You can't cancel after a vote has been cast.");
+
+	vote.cancel();
+	del(vote.get_id());
+}
+
+
 void Voting::worker()
 {
 	while(1)
@@ -117,4 +145,151 @@ void Voting::del(const decltype(votes.begin()) &it)
 	deindex(chanidx,vote.get_chan_name());
 	deindex(useridx,vote.get_user_acct());
 	votes.erase(it);
+}
+
+
+void Voting::for_each(const std::function<void (Vote &vote)> &closure)
+{
+	for(const auto &p : votes)
+	{
+		Vote &vote = *p.second;
+		closure(vote);
+	}
+}
+
+
+void Voting::for_each(const User &user,
+                      const std::function<void (Vote &vote)> &closure)
+{
+	auto pit = useridx.equal_range(user.get_acct());
+	for(; pit.first != pit.second; ++pit.first) try
+	{
+		const auto &id = pit.first->second;
+		Vote &vote = *votes.at(id);
+		closure(vote);
+	}
+	catch(const std::out_of_range &e)
+	{
+		std::cerr << "Voting index user[" << user.get_acct() << "]: "
+		          << e.what() << std::endl;
+	}
+}
+
+
+void Voting::for_each(const Chan &chan,
+                      const std::function<void (Vote &vote)> &closure)
+{
+	auto pit = chanidx.equal_range(chan.get_name());
+	for(; pit.first != pit.second; ++pit.first) try
+	{
+		const auto &id = pit.first->second;
+		Vote &vote = *votes.at(id);
+		closure(vote);
+	}
+	catch(const std::out_of_range &e)
+	{
+		std::cerr << "Voting index chan[" << chan.get_name() << "]: "
+		          << e.what() << std::endl;
+	}
+}
+
+
+void Voting::for_each(const std::function<void (const Vote &vote)> &closure)
+const
+{
+	for(const auto &p : votes)
+	{
+		const Vote &vote = *p.second;
+		closure(vote);
+	}
+}
+
+
+void Voting::for_each(const User &user,
+                      const std::function<void (const Vote &vote)> &closure)
+const
+{
+	auto pit = useridx.equal_range(user.get_acct());
+	for(; pit.first != pit.second; ++pit.first) try
+	{
+		const auto &id = pit.first->second;
+		const Vote &vote = *votes.at(id);
+		closure(vote);
+	}
+	catch(const std::out_of_range &e)
+	{
+		std::cerr << "Voting index user[" << user.get_acct() << "]: "
+		          << e.what() << std::endl;
+	}
+}
+
+
+void Voting::for_each(const Chan &chan,
+                      const std::function<void (const Vote &vote)> &closure)
+const
+{
+	auto pit = chanidx.equal_range(chan.get_name());
+	for(; pit.first != pit.second; ++pit.first) try
+	{
+		const auto &id = pit.first->second;
+		const Vote &vote = *votes.at(id);
+		closure(vote);
+	}
+	catch(const std::out_of_range &e)
+	{
+		std::cerr << "Voting index chan[" << chan.get_name() << "]: "
+		          << e.what() << std::endl;
+	}
+}
+
+
+Vote &Voting::get(const id_t &id)
+try
+{
+	return *votes.at(id);
+}
+catch(const std::out_of_range &e)
+{
+	throw Exception("There is no active vote with that ID.");
+}
+
+
+const Vote &Voting::get(const id_t &id)
+const try
+{
+	return *votes.at(id);
+}
+catch(const std::out_of_range &e)
+{
+	throw Exception("There is no active vote with that ID.");
+}
+
+
+const Voting::id_t &Voting::get_id(const User &user)
+const
+{
+	if(num_votes(user) > 1)
+		throw Exception("There are multiple votes initiated by this user. Specify an ID#.");
+
+	const auto it = useridx.find(user.get_acct());
+	if(it == useridx.end())
+		throw Exception("There are no active votes initiated by this user.");
+
+	const auto &id = it->second;
+	return id;
+}
+
+
+const Voting::id_t &Voting::get_id(const Chan &chan)
+const
+{
+	if(num_votes(chan) > 1)
+		throw Exception("There are multiple votes active in this channel. Specify an ID#.");
+
+	const auto it = chanidx.find(chan.get_name());
+	if(it == chanidx.end())
+		throw Exception("There are no active votes in this channel.");
+
+	const auto &id = it->second;
+	return id;
 }
