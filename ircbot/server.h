d813a4405ea3a7d1bbf391e484da5e2e0793e13b
jzk <jzk@freenode.memoserv>
2014-10-19 00:47:32 -0700

    Parse MAXLIST related.

--- ircbot/server.h
+++ ircbot/server.h
@@ -38,6 +38,10 @@ struct Server
 	// 3.2 CHANLIMIT
 	size_t get_chanlimit(const char &prefix) const;
 
+	// 3.10 MAXLIST
+	std::string get_maxlist_assoc(const char &prefix) const;
+	size_t get_maxlist(const char &prefix) const;
+
 	// 3.14 PREFIX
 	bool has_prefix(const char &prefix) const;
 	char prefix_to_mode(const char &prefix) const;
@@ -88,6 +92,37 @@ const try
 catch(const std::out_of_range &e)
 {
 	throw Exception("No mode for that prefix on this server.");
+}
+
+
+inline
+size_t Server::get_maxlist(const char &prefix)
+const
+{
+	for(const auto &tok : tokens(isupport["MAXLIST"],","))
+	{
+		const auto kv = split(tok,":");
+		if(kv.first.find(prefix) != std::string::npos)
+			return kv.second.empty()? std::numeric_limits<size_t>::max():
+			                          lex_cast<size_t>(kv.second);
+	}
+
+	return std::numeric_limits<size_t>::max();
+}
+
+
+inline
+std::string Server::get_maxlist_assoc(const char &prefix)
+const
+{
+	for(const auto &tok : tokens(isupport["MAXLIST"],","))
+	{
+		const auto kv = split(tok,":");
+		if(kv.first.find(prefix) != std::string::npos)
+			return kv.first;
+	}
+
+	return {};
 }
 
 
